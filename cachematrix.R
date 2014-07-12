fca23a88576ddc4efa5b9b2d7f8a7e7ccb4f8edd
Martin Isaksson <martin.isaksson@gmail.com>
2014-07-13 00:04:39 +0200

    Cached inverse of matrix
    makeCacheMatrix with set,get, setinverse, getinverse
    cacheSolve calculates inverse using solve is inverse from object is
    null, otherwise it returns the cached inverse

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,49 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  inverse <- NULL
+
+  # Set matrix, set inverse to NULL
+  set <- function(y) {
+          x <<- y
+          inverse <<- NULL
+  }
+
+  # Return matrix
+  get <- function() x
+
+  # Setter and getter for inverse
+  setinverse <- function(i) inverse <<- i
+  getinverse <- function() inverse
+
+  list(set = set, get = get,
+     setinverse = setinverse,
+     getinverse = getinverse)
+
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+
+  # Get the inverse from the x object
+  inverse <- x$getinverse()
+
+  # If the returned value is not null then we shall
+  # return this value.
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+
+  # If nothing is returned above, the inverse was null
+  # and so we should calculate it.
+  data <- x$get()
+  i <- solve(data)
+  x$setinverse(i)
+
+  # Return the inverse
+  i
 }
